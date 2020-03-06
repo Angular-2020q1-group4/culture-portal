20e8a096ef93ce0ea546ecf650fe53ef2c5024ff
Pavel232 <panq.1991@gmail.com>
2020-03-06 23:04:50 +0300

    init: start styles task

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { Error404PageComponent } from './pages/error404-page/error404-page.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { HeaderComponent } from './pages/header/header.component';
 
 @NgModule({
-  declarations: [Error404PageComponent, MainPageComponent],
-  imports: [CommonModule]
+  declarations: [Error404PageComponent, MainPageComponent, HeaderComponent],
+  imports: [CommonModule],
+  exports: [HeaderComponent, MainPageComponent]
 })
 export class CoreModule {}
