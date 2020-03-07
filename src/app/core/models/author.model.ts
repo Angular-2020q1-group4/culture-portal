6cc7801a9b00064339a22a3334f46a50bcd3f1f4
Vladimir Tyagunov <tyagunov19@gmail.com>
2020-03-07 21:46:41 +0300

    fix: fix Author and Mark models

--- src/app/core/models/author.model.ts
+++ src/app/core/models/author.model.ts
@@ -1,27 +1,27 @@
-export interface IAuthor {
-  id: number; // уникальный id, можно сделать string, тогда их надо генерировать 
-  firstName: string;// имя
+export interface Author {
+  id: number; // уникальный id, можно сделать string, тогда их надо генерировать
+  firstName: string; // имя
   middleName: string; // отчество
   lastName: string; // фамилия
   avatarImage: string;
   birthday: string; // data в виде чисел
   death?: string;
-  biography: IEvent[]; // каждое событие отображается в timeline
-  workList: IEvent[];
+  biography: Event[]; // каждое событие отображается в timeline
+  workList: Event[];
   galleryImages?: string[]; // можно оставить только ссылки на фото
   galleryVideo?: string[];
-  mapPlace?: IMark[];
+  mapPlace?: Mark[];
 }
 // метки на карте
-export interface IMark {
-  latitude: string;
-  longitude: string;
+export interface Mark {
+  latitude: number;
+  longitude: number;
   title: string; // место с описанием как оно связано с автором
 }
 
 // единая структура для биографии и работ
-export interface IEvent {
+export interface Event {
   firstDate: string;
   lastDate?: string; // для работ будет пустое
-  nameWork: string;  
+  nameWork: string;
 }
