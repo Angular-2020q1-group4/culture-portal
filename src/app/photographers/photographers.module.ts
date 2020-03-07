55d63e7dbbeffdca8a970af1619a400c37d5f09a
Alena Vasilchenko <vasilchenko.ale@gmail.com>
2020-03-07 23:59:01 +0300

    feat: add timeline component

--- src/app/photographers/photographers.module.ts
+++ src/app/photographers/photographers.module.ts
@@ -5,11 +5,15 @@ import { PhotographersRoutingModule } from './photographers-routing.module';
 import { PhotographersListPageComponent } from './pages/photographers-list-page/photographers-list-page.component';
 import { PhotographerDetailPageComponent } from './pages/photographer-detail-page/photographer-detail-page.component';
 
+import { MglTimelineModule } from 'angular-mgl-timeline.9';
+import { TimelineComponent } from './components/timeline/timeline.component';
+
 @NgModule({
   declarations: [
     PhotographersListPageComponent,
-    PhotographerDetailPageComponent
+    PhotographerDetailPageComponent,
+    TimelineComponent
   ],
-  imports: [CommonModule, PhotographersRoutingModule]
+  imports: [CommonModule, PhotographersRoutingModule, MglTimelineModule]
 })
 export class PhotographersModule {}
