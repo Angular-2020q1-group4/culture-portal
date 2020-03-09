2de8dde92ff7b0ba79eaeaaedb55f7a61627abae
Pavel232 <panq.1991@gmail.com>
2020-03-10 00:25:23 +0300

    feat: add search component

---
+++ src/app/about/components/team-user-card/team-user-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-team-user-card',
+  templateUrl: './team-user-card.component.html',
+  styleUrls: ['./team-user-card.component.scss']
+})
+export class TeamUserCardComponent implements OnInit {
+  // @Input() teamUser: TeamUser;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/photographers/photographers.module.ts
+++ src/app/photographers/photographers.module.ts
@@ -8,13 +8,15 @@ import { TimelineComponent } from './components/timeline/timeline.component';
 import { PhotographerItemComponent } from './components/photographer-item/photographer-item.component';
 import { PhotographersListPageComponent } from './pages/photographers-list-page/photographers-list-page.component';
 import { PhotographerDetailPageComponent } from './pages/photographer-detail-page/photographer-detail-page.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     TimelineComponent,
     PhotographersListPageComponent,
     PhotographerDetailPageComponent,
-    PhotographerItemComponent
+    PhotographerItemComponent,
+    SearchComponent
   ],
   imports: [SharedModule, PhotographersRoutingModule, MglTimelineModule]
 })
