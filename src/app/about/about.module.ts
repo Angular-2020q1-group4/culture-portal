9b24cc3c6325f2f72a5ace7b835c58c35f755364
Pavel232 <panq.1991@gmail.com>
2020-03-10 23:20:30 +0300

    feat: add team member card prototype

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -7,13 +7,15 @@ import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { StyleguidePageComponent } from './pages/styleguide-page/styleguide-page.component';
 import { TeamPageComponent } from './pages/team-page/team-page.component';
 import { WorklogPageComponent } from './pages/worklog-page/worklog-page.component';
+import { TeamMemberCardComponent } from './components/team-member-card/team-member-card.component';
 
 @NgModule({
   declarations: [
     AboutPageComponent,
     StyleguidePageComponent,
     TeamPageComponent,
-    WorklogPageComponent
+    WorklogPageComponent,
+    TeamMemberCardComponent
   ],
   imports: [AboutRoutingModule, SharedModule]
 })
