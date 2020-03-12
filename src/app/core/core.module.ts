3f7eb4aa28bf82103bb6103f45c5475562ec6f8e
Vladimir Tyagunov <tyagunov19@gmail.com>
2020-03-12 14:57:25 +0300

    fix: update imports

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,16 +1,9 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '@shared/shared.module';
 import { Error404PageComponent } from './pages/error404-page/error404-page.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
-
-import { registerLocaleData } from '@angular/common';
-import localeRu from '@angular/common/locales/ru';
-import localeBe from '@angular/common/locales/be';
-
-import { SharedModule } from '../shared/shared.module';
-
-//registerLocaleData(localeRu, 'ru');
 
 @NgModule({
   declarations: [Error404PageComponent, MainPageComponent],
