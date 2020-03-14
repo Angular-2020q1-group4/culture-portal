05ad8fcf9174f2fb553cbf76dfcb96352b4bc453
Alena Vasilchenko <vasilchenko.ale@gmail.com>
2020-03-14 16:15:02 +0300

    feat: add map component

--- src/app/photographers/photographers.module.ts
+++ src/app/photographers/photographers.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 import { PhotographersRoutingModule } from './photographers-routing.module';
 import { MglTimelineModule } from 'angular-mgl-timeline.9';
+import { AngularYandexMapsModule } from 'angular8-yandex-maps';
 
 import { PhotographersListPageComponent } from './pages/photographers-list-page/photographers-list-page.component';
 import { PhotographerDetailPageComponent } from './pages/photographer-detail-page/photographer-detail-page.component';
@@ -13,6 +14,8 @@ import { PhotographerCardComponent } from './components/photographer-card/photog
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { FbTransLoaderFactory } from '../firebase.module';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { MapComponent } from './components/map/map.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -21,12 +24,14 @@ import { AngularFireDatabase } from 'angularfire2/database';
     PhotographerDetailPageComponent,
     PhotographerItemComponent,
     SearchComponent,
-    PhotographerCardComponent
+    PhotographerCardComponent,
+    MapComponent
   ],
   imports: [
     SharedModule,
     PhotographersRoutingModule,
     MglTimelineModule,
+    AngularYandexMapsModule.forRoot(environment.ymapsApiKey),
     TranslateModule.forChild({
       loader: {
         provide: TranslateLoader,
