a989e5ce2cf04f1f780df20a0cf3b5abec458fdc
Alena Vasilchenko <vasilchenko.ale@gmail.com>
2020-03-14 14:42:11 +0300

    feat: modify card and timeline

--- src/app/photographers/components/photographer-card/photographer-card.component.ts
+++ src/app/photographers/components/photographer-card/photographer-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Author } from '../../../core/models/author.model';
+import { Author } from '@core/models';
 
 @Component({
   selector: 'app-photographer-card',
--- src/app/photographers/components/timeline/timeline.component.ts
+++ src/app/photographers/components/timeline/timeline.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Event } from '../../../core/models';
+import { Event } from '@core/models';
 
 @Component({
   selector: 'app-timeline',
