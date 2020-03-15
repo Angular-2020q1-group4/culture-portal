10f1033946cd124d07b3c25cf09cc232f38b82ed
Vladimir Tyagunov <tyagunov19@gmail.com>
2020-03-15 20:44:15 +0300

    Fix: fix prod environment config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    authDomain: 'culture-portal-ps.firebaseapp.com',
+    databaseURL: 'https://culture-portal-ps.firebaseio.com',
+    projectId: 'culture-portal-ps',
+    storageBucket: 'culture-portal-ps.appspot.com',
+    messagingSenderId: '34522916040',
+    appId: '1:34522916040:web:b252f5c1280458c8dcf1fd'
+  },
+  ymapsApiKey: 'ceb201b8-ea7d-4b31-bf76-66ed892d024b'
 };
