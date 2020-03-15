17eafee24270dc7983845f9fd59c912b3f18661a
Piotr Setsko <lihadzey@gmail.com>
2020-03-15 20:38:16 +0300

    feat: add footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,20 +3,22 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { AngularMaterialModule } from './angular-material.module';
 
 import { TranslateModule } from '@ngx-translate/core';
 import { DateTranslatePipe } from './pipes/date-translate.pipe';
 
 @NgModule({
-  declarations: [HeaderComponent, DateTranslatePipe],
+  declarations: [HeaderComponent, DateTranslatePipe, FooterComponent],
   imports: [CommonModule, TranslateModule, RouterModule, AngularMaterialModule],
   exports: [
     CommonModule,
     TranslateModule,
     HeaderComponent,
     AngularMaterialModule,
-    DateTranslatePipe
+    DateTranslatePipe,
+    FooterComponent
   ]
 })
 export class SharedModule {}
